bfc5be8bf1e48831a5e4588197c7822dc6c0aab2
Álvaro Cascajosa Fernández <alvaro.cf@proton.me>
2024-08-16 12:11:17 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Startpage
+
+An iTerm-inspired startpage page.
+
+## Usage
+
+- `search` or `s` + query to search in DuckDuckGo.
+- `ls` to list all shortcuts.
+- To visit a shortcut, just enter the name of the shortcut in and you'll be redirected.
+  - Example: `pro` will redirect to Proton Mail.
+  - **NOTE**: If the first few letters are shared with another shortcut, the first one will be used, needing to be more specific. Case example: `git` with GitHub and GitLab.
+- Run `weather set` to setup the `weather` command.
+- List all available commands with `help`.
---
+++ js/shortcuts.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    category: "dev",
+    color: "cyan",
+    items: {
+      github: "https://github.com/acf0x",
+      gitlab: "https://gitlab.com/",
+      aws: "https://aws.amazon.com",
+      "conventional commits": "https://www.conventionalcommits.org/en/v1.0.0/#summary",
+    },
+  },
+  {
+    category: "work",
+    color: "green",
+    items: {
+      linkedin: "https://linkedin.com/feed",
+      glassdoor: "https://www.glassdoor.es/Opiniones/index.htm",
+      resources: "https://free-for.dev/#/",
+      "code ss": "https://app.codeimage.dev/",
+    },
+  },
+  {
+    category: "coms",
+    color: "red",
+    items: {
+      proton: "https://mail.proton.me/u/1/inbox",
+      gmail: "https://mail.google.com/mail/u/0/#inbox",
+      whatsapp: "https://web.whatsapp.com/",
+      telegram: "https://web.telegram.org/",
+    },
+  },
+  {
+    category: "misc",
+    color: "purple",
+    items: {
+      youtube: "https://www.youtube.com/feed/subscriptions?flow=2",
+      twitch: "https://www.twitch.tv/directory/following/live",
+      tempmail: "https://tempmail.email/",
+      "/g/": "https://boards.4chan.org/g/",      // IPTorrents: "https://iptorrents.com/t",
+    },
+  },
+];
